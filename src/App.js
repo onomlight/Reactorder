1419822ffb722b4b0e48355fc1c6fee5dc6a2801
onomlight <pass041@nate.com>
2023-01-09 05:49:13 +0900

    더미데이터불러옥

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react'
 import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
 
 export default function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   )
 }
