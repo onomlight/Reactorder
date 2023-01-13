986220bc98d3400b8f4aab76411be62a19d61bc0
onomlight <pass041@nate.com>
2023-01-13 13:21:27 +0900

    cart 추가

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import classes from './Cart.module.css'
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-itmes']}>
+      {[{ id: 'c1', name: 'seeri', amout: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  )
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>총 금액</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>닫기</button>
+        <button className={classes.button}>주문 </button>
+        {/* 우선은 더미데이터로 렌더링  */}
+      </div>
+    </div>
+  )
+}
+
+export default Cart
